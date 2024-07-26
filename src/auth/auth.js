5ed7382c36cc3716dba4145f804c0da24015ceab
ItzTas <talessabini2@gmail.com>
2024-07-26 19:55:27 -0300

    feat: authentication and hash

---
+++ src/auth/auth.js
@@ -0,0 +1,34 @@
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+
+function generateSalt(length = 16) {
+  return crypto.randomBytes(length).toString("hex");
+}
+
+const PEPPER = process.env["PEPPER"];
+
+/**
+ *
+ * @param {String} password
+ * @returns {Promise<Object>}
+ */
+async function hashPassword(password) {
+  const saltRounds = 10;
+  const salt = generateSalt(16);
+  const pass = password + PEPPER + salt;
+  const hashedPassword = await bcrypt.hash(pass, saltRounds);
+  return { hashedPassword, salt };
+}
+
+/**
+ *
+ * @param {String} password
+ * @param {String} salt
+ * @param {String} hash
+ */
+async function compareHashFromPassword(password, salt, hash) {
+  const pass = password + PEPPER + salt;
+  return await bcrypt.compare(pass, hash);
+}
+
+export { hashPassword, compareHashFromPassword };
