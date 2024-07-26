750ea8678ff43221f2d4153aca3b025fd5951b29
ItzTas <talessabini2@gmail.com>
2024-07-26 20:26:18 -0300

    feat: create user handler implemented

--- src/controllers/users_controlers.js
+++ src/controllers/users_controlers.js
@@ -3,11 +3,45 @@
  * @typedef {import('express').Response} Response
  */
 
+import { StatusCodes } from "http-status-codes";
+import {
+  createDatabaseUser,
+  ErrorAlreadyExists,
+} from "../database/users_queries.js";
+import { databaseUserToUser } from "../models/app_users.js";
+
 /**
  *
  * @param {Request} req
  * @param {Response} res
  */
-function handlerCreateUser(req, res) {}
+async function handlerCreateUser(req, res) {
+  const { real_name, user_name, email, password } = req.body;
+  if (!real_name || !user_name || !email || !password) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      error: `unfilfilled params ${req.body}`,
+    });
+  }
+
+  let dbUser;
+
+  try {
+    dbUser = await createDatabaseUser(real_name, user_name, email, password);
+  } catch (err) {
+    if (err instanceof ErrorAlreadyExists) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        error: "Given user already exists",
+        error_infos: err,
+      });
+    }
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: "could not create user",
+      error_infos: err,
+    });
+  }
+
+  // databaseUserToUser hides secure user information
+  return res.status(StatusCodes.CREATED).json(databaseUserToUser(dbUser));
+}
 
 export { handlerCreateUser };
