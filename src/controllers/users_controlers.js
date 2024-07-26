e72e4efb6a7cb7cdf04239a9948a1d77696f7e80
ItzTas <talessabini2@gmail.com>
2024-07-26 20:18:26 -0300

    chore: endpoint post /users/accounts

---
+++ src/controllers/users_controlers.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {import('express').Request} Request
+ * @typedef {import('express').Response} Response
+ */
+
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ */
+function handlerCreateUser(req, res) {}
+
+export { handlerCreateUser };
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { handlerCreateUser } from "../controllers/users_controlers.js";
 
 const router = Router();
+
+router.post("/users/accounts", handlerCreateUser);
 
 export default router;
