b5416d36a104c8fb98fa840d5c775be4da84f2ce
ItzTas <talessabini2@gmail.com>
2024-07-27 01:44:44 -0300

    chore: dockerize app

--- src/database/database_client.js
+++ src/database/database_client.js
@@ -1,11 +1,11 @@
 import pkg from "pg";
 const { Pool, Client } = pkg;
 
-const USER = process.env["DB_USER"];
+const USER = process.env["POSTGRES_USER"];
 const DB_PORT = process.env["DB_PORT"];
-const PASSWORD = `${process.env["DB_PASSWORD"]}`;
-const HOST = process.env["HOST"];
-const DB_NAME = process.env["DB_NAME"];
+const PASSWORD = `${process.env["POSTGRES_PASSWORD"]}`;
+const HOST = process.env["DB_HOST"];
+const DB_NAME = process.env["POSTGRES_DB"];
 
 const poolConfigs = {
   host: HOST,
