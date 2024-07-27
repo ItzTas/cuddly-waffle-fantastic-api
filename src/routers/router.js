66ea5ef52e23318644d7ee593c4305156f92947c
ItzTas <talessabini2@gmail.com>
2024-07-26 23:21:24 -0300

    chore: /api/users/:email/email endpoint

--- src/routers/router.js
+++ src/routers/router.js
@@ -3,6 +3,7 @@ import {
   handlerCreateUser,
   handlerGetAllUsers,
   handlerGetUserById,
+  handlerGetUserByEmail,
   handlerUpdateAllUserInfosById,
 } from "../controllers/users_controllers.js";
 
@@ -11,6 +12,7 @@ const router = Router();
 router.post("/api/users/accounts", handlerCreateUser);
 router.get("/api/users", handlerGetAllUsers);
 router.get("/api/users/:id/id", handlerGetUserById);
+router.get("/api/users/:email/email", handlerGetUserByEmail);
 router.patch("/api/users/:id/id", handlerUpdateAllUserInfosById);
 
 export default router;
