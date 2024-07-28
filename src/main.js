d7ef12114ec8af07450077e8c20f924018656ee0
ItzTas <talessabini2@gmail.com>
2024-07-27 23:46:58 -0300

    refactor: new env var

--- src/main.js
+++ src/main.js
@@ -4,13 +4,18 @@ import router from "./routers/router.js";
 
 const app = express();
 
+const defaultTokenExpiration = "3h";
+
 app.use(express.json());
 app.use(router);
 
 const PORT = process.env["PORT"];
+const HOST = process.env["HOST"];
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}: http://localhost:${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}: http://${HOST}:${PORT}`);
 });
 
 export default app;
+
+export { defaultTokenExpiration, server };
